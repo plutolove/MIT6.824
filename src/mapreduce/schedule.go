041b8b59789601430ab5b1da0c744b56f4f1a774
plutolove <mengshangqi@gmail.com>
2017-03-29 13:53:20 +0800

    Distributing MapReduce tasks

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -23,6 +26,40 @@ func (mr *Master) schedule(phase jobPhase) {
 	// multiple tasks.
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+
+	var mutex sync.WaitGroup
+
+	for i := 0; i < ntasks; i++ {
+		mutex.Add(1)
+		go func(taskid int, nfiles int, phase jobPhase) { // create a goroutine for a task
+			defer mutex.Done()
+			debug("DEBUG: current taskNum: %v, nios: %v, phase: %v\n", taskid, nios, phase)
+
+			for {
+				worker_ip := <- mr.registerChannel // get an unuse worker
+
+				var args DoTaskArgs
+				args.File = mr.files[taskid]
+				args.JobName = mr.jobName
+				args.NumOtherPhase = nios
+				args.Phase = phase
+				args.TaskNumber = taskid
+
+				is_success := call(worker_ip, "Worker.DoTask", &args, new(struct{})) // run worker with rpc
+
+				if is_success { // iif is success free the worker and put it in registerChannel, then break the loop
+					go func() {
+						mr.registerChannel <- worker_ip
+					}()
+					break
+				}
+			}
+
+		}(i, nios, phase)
+	}
+
+	mutex.Wait()
+
 	//
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
